a22b715e567db4551c9d133883488b56d3da0af9
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2024-07-03 16:33:41 +0700

    update background

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#F3F8FF]`}>{children}</body>
+      <body className={`${inter.className} bg-[#FFF]`}>{children}</body>
     </html>
   );
 }
